0dba4e61ac84f61430d2f0486543497a868774a4
JonathanPereira <jonathan@jonathanpereira.com>
2019-01-04 11:12:02 +0100

    Episode 8 enhancements

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,48 +1,27 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Bird Board</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css"
-          integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-</head>
-<body>
+@extends('layouts.app')
 
-<div class="container">
-
+@section('content')
     <h1>Create a project</h1>
-
-    <div class="container">
-        <form method="POST" action="/projects">
-            @csrf
-            <div class="form-group">
-                <label for="title" class="col-sm-1-12 col-form-label">Title</label>
-                <div class="col-sm-1-12">
-                    <input type="text"
-                           class="form-control" name="title" id="title" placeholder="">
-                </div>
+    <form method="POST" action="/projects">
+        @csrf
+        <div class="form-group">
+            <label for="title" class="col-sm-1-12 col-form-label">Title</label>
+            <div class="col-sm-1-12">
+                <input type="text"
+                       class="form-control" name="title" id="title" placeholder="">
             </div>
-            <div class="form-group">
-                <label for="description">Description</label>
-                <textarea class="form-control" name="description" id="description" rows="3"></textarea>
+        </div>
+        <div class="form-group">
+            <label for="description">Description</label>
+            <textarea class="form-control" name="description" id="description" rows="3"></textarea>
+        </div>
+        <div class="form-group">
+            <div class="">
+                <button type="submit" class="btn btn-primary">Add</button>
+                <a href="/projects">Cancel</a>
             </div>
-            <div class="form-group row">
-                <div class="offset-sm-2 col-sm-10">
-                    <button type="submit" class="btn btn-primary">Add</button>
-                </div>
-            </div>
-        </form>
-    </div>
+        </div>
+    </form>
 
-</div>
 
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"
-        integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1"
-        crossorigin="anonymous"></script>
-</body>
-</html>
+@endsection

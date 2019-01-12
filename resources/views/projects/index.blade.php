a7fe2abe8b948c99636f4342c9f9d52b922808ac
JonathanPereira <jonathan@jonathanpereira.com>
2019-01-12 12:18:42 +0100

    Episode 10 enhancements

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,24 +2,35 @@
 
 @section('content')
 
-        <div class="flex itens-center mb-3">
-            <a href="/projects/create">New project</a>
-        </div>
+        <header class="flex itens-center justify-between mb-3 py-3">
 
-        <div class="flex">
+            <h2 class="text-grey text-sm font-normal">My Projects</h2>
+
+            <a href="/projects/create" class="button">New project</a>
+
+        </header>
+
+        <main class="lg:flex lg:flex-wrap -mx-3">
 
             @forelse($projects as $project)
-                <div class="bg-white mr-4 p-5 rounded shadow w-1/3" style="height: 200px;">
 
-                    <h3 class="font-normal text-lg py-4">{{ $project->title }}</h3>
+                <div class="lg:w-1/3 px-3 pb-6">
 
-                    <div class="text-grey">{{ str_limit($project->description, 100) }}</div>
+                    <div class="bg-white p-5 rounded-lg shadow" style="height: 200px;">
+
+                        <h3 class="font-normal text-lg py-4 -ml-5 mb-3 border-l-4 border-blue-light pl-4">
+                            <a href="{{ $project->path() }}" class="text-black no-underline">{{ $project->title }}</a>
+                        </h3>
+
+                        <div class="text-grey">{{ str_limit($project->description, 100) }}</div>
+
+                    </div>
 
                 </div>
             @empty
                 <div>No projects yet.</div>
             @endforelse
 
-        </div>
+        </main>
 
 @endsection
